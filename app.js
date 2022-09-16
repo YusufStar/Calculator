e9ca1eb24f01b8467d619a96cf048d5ceb0078ff
Yusuf Yıldız <ysfyldz0077@gmail.com>
2022-09-16 16:03:24 +0300

    add keyboard control

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 start = true
 
+keyboardkeys = ["0","1","2","3","4","5","6","7","8","9",'+','-','*','/',]
+
 display = document.getElementsByClassName("calculator__display")[0];
 
 buttons = Array.from(document.querySelectorAll("button"));
@@ -25,4 +27,28 @@ buttons.map( button => {
                 display.innerText += e.target.innerText
         }
     });
+});
+
+document.addEventListener("keydown", (k) => {
+    console.log(k)
+    for(i = 0; i < keyboardkeys.length; i++) {
+        if(k.key == keyboardkeys[i]) {
+            if(start == true) {
+                display.innerText = ""
+                start = false
+            }
+            switch(k.key) {
+                default:
+                    display.innerText += k.key
+            }
+        } else if (k.key == 'Enter') {
+            display.innerText = eval(display.innerText)
+        } else if (k.key == "Backspace") {
+            if(display.innerText) {
+                console.log("first")
+                display.innerText = display.innerText.slice(0,- 1)
+                break
+            }
+        }
+    }
 });
